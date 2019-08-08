2e9e7ce0ef6f24a5c17703eb690ef240c645765d
h2190697689 <2190697689@qq.com>
2019-08-08 17:33:55 +0800

    remove model

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Node.js
+## 安装配置(https://jingyan.baidu.com/article/48b37f8dd141b41a646488bc.html)
--- model/bin/www.js
+++
@@ -1,6 +0,0 @@
-const http=require("http");
-
-const PORT = 8000;
-const serverHandle = require("../app");
-conser server = http.createServer(serverHandle);
-server.listen(PORT);
